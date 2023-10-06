aeaa1a7900ecf5ca21b3f0b3ec7b3a2702f8de4f
developer001 <developer001@rizsolutions.com>
2023-10-06 19:57:25 +0500

    Added a DockerFile

--- Index.js
+++ Index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import dbConnection from "./Database/Db.js";
+import dbConnection from "./database/DataBase.js";
 import Route from "./Routes/route.js";
 import cors from "cors";
 import bodyParser from "body-parser";
